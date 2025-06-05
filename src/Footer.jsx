a088a806ba6729c39b39f333a6e750f533d1765a
pushpak gupta <pushpak.gupta@kpit.com>
2025-06-05 23:19:23 +0530

    footer icon fix

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,11 +3,17 @@ import { FaLinkedin } from "react-icons/fa";
 import { FaSquareFacebook } from "react-icons/fa6";
 
 export default function Footer() {
-  return (
-    <footer className="bg-gray-800 text-white text-center py-4">
-      <p>&copy; {new Date().getFullYear()} WellTogether.in | All rights reserved  |</p>
-      <span><FaLinkedin/></span>
-      <span><FaSquareFacebook/></span>
+  return ( 
+    <footer className="bg-gray-800 text-white text-center py-4 text-[10px] md:text-2xl">
+      <p className="flex items-center justify-center space-x-4">
+        <span>&copy; {new Date().getFullYear()} WellTogether.in | All rights reserved |</span>
+        <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer" className="hover:text-blue-500">
+          <FaLinkedin />
+        </a>
+        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="hover:text-blue-500">
+          <FaSquareFacebook />
+        </a>
+      </p>
     </footer>
   );
 }
