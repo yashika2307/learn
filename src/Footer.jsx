7dfc2a8753e07c8f659e8cac01701947b82bbeb4
Yashika Gupta <yashika2307@gmail.com>
2025-08-26 00:56:43 +0530

    meta tag n image

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FaLinkedin } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 import { FaSquareFacebook } from "react-icons/fa6";
 
 export default function Footer() {
@@ -7,10 +8,14 @@ export default function Footer() {
     <footer className="bg-gray-800 text-white text-center py-4 text-[10px] md:text-2xl">
       <p className="flex items-center justify-center space-x-4">
         <span>&copy; {new Date().getFullYear()} WellTogether.in | All rights reserved |</span>
-        <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer" className="hover:text-blue-500">
+        
+        <a href="https://www.instagram.com/welltogetherin?igsh=MTJvMWNrNTVrMHo3bg==" target="_blank" rel="noopener noreferrer" className="hover:text-blue-500">
+          <FaInstagram />
+        </a>
+        <a href="https://www.linkedin.com/company/well-together-in/" target="_blank" rel="noopener noreferrer" className="hover:text-blue-500">
           <FaLinkedin />
         </a>
-        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="hover:text-blue-500">
+        <a href="https://www.facebook.com/welltogetherin" target="_blank" rel="noopener noreferrer" className="hover:text-blue-500">
           <FaSquareFacebook />
         </a>
       </p>
